e6e528226585c6d010ff5480456790ff80a194f0
spiderasasen <spiderasasen@gmail.com>
2026-04-22 11:38:00 -0400

    made the basic bank ui

--- bank.go
+++ bank.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv" //makes it possible to print out integers in the print statment
+)
+
+// import vars here
+var amount int = 0
+var onHand int = 2000
+var options []string = []string{"a. deposit", "b. withdraw", "c. logout"}
 
 func mainBank(user string, pass string) bool {
+	//printing out the user and the password
 	fmt.Println("Username: " + user)
 	fmt.Println("Password: " + pass)
+
+	//showing the amount and the options
+	fmt.Println("\nAmount: " + strconv.Itoa(amount) + "\n\nPlease select a option")
+	for option := range options {
+		fmt.Println(options[option])
+	}
+
 	return true
 }
