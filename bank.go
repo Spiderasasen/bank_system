3f53290233ef7c4734e1a534e399da925c4238eb
spiderasasen <spiderasasen@gmail.com>
2026-04-22 11:13:45 -0400

    got the main system working in bank.go

---
+++ bank.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func mainBank(user string, pass string) bool {
+	fmt.Println("Username: " + user)
+	fmt.Println("Password: " + pass)
+	return true
+}
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	//boolean for loop
 	var login bool
@@ -13,6 +11,9 @@ func main() {
 	//getting the username and password
 	var username string = getUsername()
 	var password string = getPassword()
-	fmt.Println(username)
-	fmt.Println(password)
+
+	var bank bool
+	for !bank {
+		bank = mainBank(username, password)
+	}
 }
